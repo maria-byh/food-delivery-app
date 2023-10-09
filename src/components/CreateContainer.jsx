68eccb2f6729bfd4b695d5a59f4f8a755fb5152b
maria-byh <maria.univ13@gmail.com>
2023-10-09 16:24:14 +0100

    new changes

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { motion } from 'framer-motion';
 
-import { MdFastfood } from 'react-icons/md';
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from 'react-icons/md';
 import { categories } from '../utils/Data';
 import Loader from './Loader';
 
@@ -16,6 +16,10 @@ export default function CreateContainer() {
   const [alertStatus, setAlertStatus] = useState('danger');
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const uploadImage = () => {}
+  const deleteImage = () => {}
+  const saveDetails = () => {}
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
@@ -43,7 +47,7 @@ export default function CreateContainer() {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder='Give me a title...'
-            className='w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor'
+            className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor'
           />
         </div>
         <div className='w-full'>
@@ -62,7 +66,50 @@ export default function CreateContainer() {
           </select>
         </div>
         <div className='group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg'>
-          {isLoading ? <Loader/> : <></>}
+          {isLoading ? <Loader/> : <>
+              {!imageAsset ? 
+              <>
+                <label className='w-full h-full flex flex-col items-center justify-center cursor-pointer'>
+                  <div className='w-full h-full flex flex-col items-center justify-center gap-2'>
+                    <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700'/>
+                    <p className='text-gray-500 hover:text-gray-700'>click here to upload</p>
+                  </div>
+                  <input type="file" name='uploadimage' accept='image/*' onChange={uploadImage} className='w-0 h-0'/>
+                </label>
+              </> 
+              : <>
+                <div className='relative h-full'>
+                  <img src={imageAsset} alt="aploaded image" className='w-full h-full object-cover'/>
+                  <button type='button' onClick={deleteImage} className='absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out'>
+                    <MdDelete className='text-white'/>
+                  </button>
+                </div>
+              </>}
+          </>}
+        </div>
+        <div className='w-full flex flex-col md:flex-row items-center gap-3'>
+          <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+            <MdFoodBank className='text-gray-700 text-2xl'/>
+            <input type="text" required 
+            placeholder='calories' 
+            value={calories}
+            onChange={(e) => setCalories(e.target.value)}
+            className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor'/>
+          </div> 
+          <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+            <MdAttachMoney className='text-gray-700 text-2xl'/>
+            <input type="text" required 
+            placeholder='price' 
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor'/>
+          </div> 
+        </div>
+        <div className='w-full flex items-center'>
+          <button type='button' onClick={saveDetails} 
+          className='ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold'>
+            Save
+          </button>
         </div>
       </div>
     </div>
