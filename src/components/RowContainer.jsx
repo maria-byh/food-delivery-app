7f04283640983441844ec19cddefbf271b58587a
maria-byh <maria.univ13@gmail.com>
2023-11-09 16:42:47 +0100

    menu container

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { MdShoppingBasket } from 'react-icons/md'
+import { motion } from 'framer-motion';
+import icecream from '../images/i6.png'
 
-const RowContainer = ({flag}) => {
+const RowContainer = ({flag, data, scrollValue}) => {
+    const rowContainer = useRef()
+
+    useEffect(() => {
+        rowContainer.current.scrollLeft += scrollValue
+    }, [scrollValue])
+
     return (
-        <div className={`w-full my-12 ${
-            flag ? "overflow-x-scroll" : "overflow-x-hidden"
+        <div
+            ref={rowContainer}
+            className={`w-full flex items-center my-12 gap-3 scroll-smooth ${
+            flag ? "overflow-x-scroll scrollbar-none" : "overflow-x-hidden flex-wrap"
         }`}>
-            <div className='w-full md:w-350 h-20 bg-cardOverlay shadow-md backdrop-blur-lg '></div>
+            {data && data.map(item => (
+                <div key={item?.id} className='w-300 min-w-[300px] my-12 h-[225px] bg-cardOverlay rounded-lg p-2 md:w-340 md:min-w-[340px] backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-between'>
+                    <div className='w-full flex items-center justify-between'>
+                        <motion.img whileHover={{scale: 1.2}} src={item?.imageURL} alt="img" className='w-40 -mt-8 drop-shadow-2xl'/>
+                        <motion.div whileTap={{scale: 0.75}}
+                        className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
+                            <MdShoppingBasket className='text-white'/>
+                        </motion.div>
+                    </div>
+                    <div className='w-full flex flex-col items-end justify-end'>
+                        <p className='text-textColor font-semibold text-base md:text-lg '>
+                            {item?.title}
+                        </p>
+                        <p className='mt-1 text-sm text-gray-500'>
+                            {item?.calories} calories
+                        </p>
+                        <div className='flex items-center gap-8'>
+                            <p className='text-lg font-semibold text-headingColor'>
+                                <span className='text-sm text-red-500 '>$</span>{item?.price}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            
         </div>
     );
 };
